0f00ee6f1ae9097f052bc96eee666f11bf357418
rangemaster <rangemaster_r@hotmail.com>
2014-12-29 15:53:54 +0100

    Added functions

--- Project_Johnwesley/Project_Johnwesley_Part1/Project_Johnwesley_Part1.Windows/BillPage.xaml.cs
+++ Project_Johnwesley/Project_Johnwesley_Part1/Project_Johnwesley_Part1.Windows/BillPage.xaml.cs
@@ -23,9 +23,11 @@ namespace Project_Johnwesley_Part1
     {
         private double _TotalMoney = 2000;
         private List<Bill> _Bills;
+        private List<TextBox> textBoxes;
         private NavigationHelper navigationHelper;
         private ObservableDictionary defaultViewModel = new ObservableDictionary();
         private Logic _Logic = null;
+        private Button _Done_bn, _Add_Bill_bn;
         public BillPage()
         {
             this.InitializeComponent();
@@ -67,10 +69,24 @@ namespace Project_Johnwesley_Part1
             this._Total_Money_tx.TextChanged += _Total_Money_tx_TextChanged;
             this._Payed_Money_tx.IsEnabled = false;
             this._InPossession_Money_tx.IsEnabled = false;
-            this._Information_Panel_1.Children.Add(CreateTextBox("Payer:"));
-            this._Information_Panel_1.Children.Add(CreateTextBox("Price"));
-            this._Information_Panel_2.Children.Add(CreateTextBox("Time:"));
-            this._Information_Panel_3.Children.Add(CreateTextBox("Description:"));
+            this.textBoxes = new List<TextBox>();
+            textBoxes.Add(CreateTextBox("Payer:"));
+            textBoxes.Add(CreateTextBox("Price"));
+            textBoxes.Add(CreateTextBox("Time:"));
+            textBoxes.Add(CreateTextBox("Description:"));
+            this._Information_Panel_1.Children.Add(textBoxes[0]);
+            this._Information_Panel_1.Children.Add(textBoxes[1]);
+            this._Information_Panel_2.Children.Add(textBoxes[2]);
+            this._Information_Panel_3.Children.Add(textBoxes[3]);
+            this._Add_Bill_bn = new Button();
+            this._Add_Bill_bn.Content = "Add";
+            this._Add_Bill_bn.Click += _Add_Bill_bn_Click;
+            this._Done_bn = new Button();
+            this._Done_bn.Content = "Done";
+            this._Done_bn.Click += _Done_bn_Click;
+            this._Information_Panel_3.Children.Add(_Done_bn);
+            InformationFieldEditable(false);
+            TopInformationFIeldVisible(true);
         }
         private void _Total_Money_tx_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -91,7 +107,6 @@ namespace Project_Johnwesley_Part1
             tb.Header = header;
             tb.Text = "";
             tb.TextWrapping = TextWrapping.Wrap;
-            tb.IsEnabled = false;
             return tb;
         }
         #endregion
@@ -115,10 +130,35 @@ namespace Project_Johnwesley_Part1
         }
         private void ClearInformationField()
         {
-            ((TextBox)this._Information_Panel_1.Children[0]).Text = "";
-            ((TextBox)this._Information_Panel_1.Children[1]).Text = "";
-            ((TextBox)this._Information_Panel_2.Children[0]).Text = "";
-            ((TextBox)this._Information_Panel_3.Children[0]).Text = "";
+            foreach (TextBox tb in textBoxes)
+            { tb.Text = ""; }
+        }
+        private void TopInformationFIeldVisible(bool result)
+        {
+            Windows.UI.Xaml.Visibility Vis = Windows.UI.Xaml.Visibility.Collapsed;
+            if (result)
+                Vis = Windows.UI.Xaml.Visibility.Visible;
+            _Total_Money_tx.Visibility = Vis;
+            _Total_Money_tx.IsEnabled = result;
+            _Payed_Money_tx.Visibility = Vis;
+            _InPossession_Money_tx.Visibility = Vis;
+            Bill_List.Visibility = Vis;
+            Bill_List.IsEnabled = result;
+            Add_bn.Visibility = Vis;
+            Add_bn.IsEnabled = result;
+            Edit_bn.Visibility = Vis;
+            Edit_bn.IsEnabled = result;
+            Remove_bn.Visibility = Vis;
+            Remove_bn.IsEnabled = result;
+            if (result) _Done_bn.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            else _Done_bn.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            _Done_bn.IsEnabled = !result;
+
+        }
+        private void InformationFieldEditable(bool result)
+        {
+            foreach (TextBox tb in textBoxes)
+            { tb.IsEnabled = result; }
         }
         #endregion
         #endregion
@@ -168,21 +208,55 @@ namespace Project_Johnwesley_Part1
         private void Edit_Flyout_Continue_bn(object sender, RoutedEventArgs e)
         {
             if (CheckAdminPassword(_Edit_Field_tx.Text))
-            { EditContinue(); HideEditFlyout(); }
+            {
+                InformationFieldEditable(true);
+                HideEditFlyout();
+                TopInformationFIeldVisible(false);
+            }
+            _Edit_Field_tx.Text = "";
         }
+        private void Close_Flyout_Continue_bn(object sender, RoutedEventArgs e)
+        { HideEditFlyout(); }
+        private void _Add_Bill_bn_Click(object sender, RoutedEventArgs e)
+        { }
+        private void _Done_bn_Click(object sender, RoutedEventArgs e)
+        { ReplaceEditedBill(); }
         #endregion
         #region Button Functions
         private void ShowEditFlyout()
-        { this._Edit_Flyout.Visibility = Visibility.Visible; }
+        {
+            if (this._Edit_Flyout.Visibility == Visibility.Visible)
+            { this._Edit_Flyout.Visibility = Visibility.Collapsed; }
+            else
+            { this._Edit_Flyout.Visibility = Visibility.Visible; }
+        }
         private void HideEditFlyout()
         { this._Edit_Flyout.Visibility = Visibility.Collapsed; }
         private bool CheckAdminPassword(string pass)
         {
+            if (pass == "1234")
+                return true;
             return false;
         }
-        private void EditContinue()
+        private void ReplaceEditedBill()
         {
-            
+            int index = this.Bill_List.SelectedIndex;
+            if (index != -1)
+            {
+                string[] array = textBoxes[2].Text.Split('-');
+                DateTime time = new DateTime(int.Parse(array[0].Trim()), int.Parse(array[1].Trim()), int.Parse(array[2].Trim()));
+                Bill bill = new Bill.BillBuilder()
+                    .Payer(textBoxes[0].Text)
+                    .Price(Double.Parse(textBoxes[1].Text))
+                    .Time(time)
+                    .Description(textBoxes[3].Text).Build();
+                this._Bills.Insert(index, bill);
+                this._Bills.RemoveAt(index + 1);
+                TopInformationFIeldVisible(true);
+                InformationFieldEditable(false);
+                Update();
+                UpdateList();
+            }
         }
         #endregion
         private void Bill_List_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -200,6 +274,12 @@ namespace Project_Johnwesley_Part1
                 }
             }
             Debug.WriteLine("Selection Changed");
+        }
+
+        private void _Edit_Field_tx_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter && _Edit_Flyout.Visibility == Windows.UI.Xaml.Visibility.Visible)
+            { Edit_Flyout_Continue_bn(sender, e); }
         }
     }
 }
